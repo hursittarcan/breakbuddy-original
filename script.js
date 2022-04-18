f3020126b5f332860c96b60396d1773246252edd
HursitTarcan <hursittarcan@gmail.com>
2022-04-18 17:43:15 +0200

    Local User Answer Data Storage + Some Refactoring

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
-const time_line = document.querySelector("header .time_line");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
 
-// if startQuiz button clicked
+//Start button on the homepage.
 start_btn.onclick = ()=>{
     //quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0);
-    info_box.classList.add("activeInfo"); //show info box
+    info_box.classList.add("activeInfo");
     home_screen.style.visibility='hidden';
 }
 
@@ -30,10 +27,8 @@ continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
 }
 
-let timeValue =  15;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -48,16 +43,12 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); //show quiz box
     result_box.classList.remove("activeResult"); //hide result box
-    timeValue = 15;
     que_count = 0;
     que_numb = 1;
     userScore = 0;
     widthValue = 0;
     showQuetions(que_count); //calling showQestions function
     queCounter(que_numb); //passing que_numb value to queCounter
-    clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
-    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
     next_btn.classList.remove("show"); //hide the next button
 }
 
@@ -70,15 +61,13 @@ const next_btn = document.querySelector("footer .next_btn");
 const bottom_ques_counter = document.querySelector("footer .total_que");
 bottom_ques_counter.innerHTML = '<span><p>'+ 1 +'</p> of <p>'+ questions.length +'</p> Questions</span>';
 
-// if Next Que button clicked
+// if Next button clicked
 next_btn.onclick = ()=>{
     if(que_count < questions.length - 1){ //if question count is less than total question length
         que_count++; //increment the que_count value
         que_numb++; //increment the que_numb value
         showQuetions(que_count); //calling showQestions function
         queCounter(que_numb); //passing que_numb value to queCounter
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
         next_btn.classList.remove("show"); //hide the next button
     }else{
         showResult(); //calling showResult function
@@ -116,8 +105,9 @@ function optionSelected(answer){
     const allOptions = option_list.children.length; //getting all option items
 
     //localStorage.setItem(key, textareaElement.value)
-    //let key = ""
-    //localstorage.setItem();
+    let key = "answer_" + questions[que_count].numb;
+    localStorage.setItem(key, userAns);
+    //alert(JSON.stringify(key));
 
     if(userAns == correcAns){ //if user selected option is equal to array's correct answer
         userScore += 1; //upgrading score value with 1
@@ -131,7 +121,7 @@ function optionSelected(answer){
         answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
         console.log("Wrong Answer");
 
-        for(i=0; i < allOptions; i++){
+        for(let i=0; i < allOptions; i++){
             if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
                 option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
                 option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
