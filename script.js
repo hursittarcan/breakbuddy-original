9f6157776e9e761b50c94832a7dcd98cc8ff6910
HursitTarcan <hursittarcan@gmail.com>
2022-04-18 20:15:33 +0200

    Refactored OptionSelected Function 1.0 Removed Multiple Ticks Bug Added Tick To Chosen Answer

--- script.js
+++ script.js
@@ -31,9 +31,6 @@ continue_btn.onclick = ()=>{
 
 let que_count = 0;
 let que_numb = 1;
-let userScore = 0;
-let counter;
-let counterLine;
 let widthValue = 0;
 
 const restart_quiz = result_box.querySelector(".buttons .restart");
@@ -96,43 +93,41 @@ function showQuetions(index){
 }
 // creating the new div tags which for icons
 let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 //if user clicked on option
 function optionSelected(answer){
     let userAns = answer.textContent; //getting user selected option
-    let correcAns = questions[que_count].answer; //getting correct answer from array
     const allOptions = option_list.children.length; //getting all option items
 
     //localStorage.setItem(key, textareaElement.value)
     let key = "answer_" + questions[que_count].numb;
     localStorage.setItem(key, userAns);
-    //alert(JSON.stringify(key));
+    answer.classList.add("correct");
+    answer.setAttribute("class", "option correct");
+    answer.insertAdjacentHTML("beforeend", tickIconTag);
 
-    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
-        userScore += 1; //upgrading score value with 1
-        answer.classList.add("correct"); //adding green color to correct selected option
-
-        console.log("Correct Answer");
-        console.log("Your correct answers = " + userScore);
-    }else{
-        answer.classList.add("incorrect"); //adding red color to correct selected option
-        //answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
-        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
-        console.log("Wrong Answer");
-
-        for(let i=0; i < allOptions; i++){
-            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                console.log("Auto selected correct answer.");
-            }
-        }
-    }
-    for(i=0; i < allOptions; i++){
+    for(let i = 0; i < allOptions; i++){
         option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
     }
     next_btn.classList.add("show"); //show the next button if user selected any option
+
+    // if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+    //      //adding green color to correct selected option
+    //
+    //     console.log("Correct Answer");
+    //     console.log("Your correct answers = " + userScore);
+    // }else{
+    //     answer.classList.add("incorrect"); //adding red color to correct selected option
+    //     //answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+    //      //adding tick icon to correct selected option
+    //     console.log("Wrong Answer");
+    //
+    //
+    // }
+    // for(i=0; i < allOptions; i++){
+    //     option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    // }
+    // next_btn.classList.add("show"); //show the next button if user selected any option
 }
 
 function showResult(){
