f1aab06de268717c3080ed63bb44aaca8eb78785
HursitTarcan <hursittarcan@gmail.com>
2022-04-22 18:47:43 +0200

    Countries.js Added + Script.js Refactored.

--- questions.js
+++ questions.js
@@ -1,4 +1,3 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
         numb: 1,
--- script.js
+++ script.js
@@ -132,30 +132,3 @@ function adviceLocation() {
     country_advise_text.innerHTML = randomCountry.name;
 }
 
-function initializeEuropeanCountries() {
-    countries.push(
-        {name: "Turkey", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Belgium", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Museum"},
-        {name: "Italy", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Spain", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Portugal", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "France", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Museum"},
-        {name: "The Netherlands", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Museum"},
-        {name: "United Kingdom", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Ireland", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Museum"},
-        {name: "Germany", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Museum"},
-        {name: "Russia", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Experience", tours: "Food"},
-        {name: "Ukraine", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Experience", tours: "Food"},
-        {name: "Sweden", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Museum"},
-        {name: "Norway", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Museum"},
-        {name: "Finland", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Museum"},
-        {name: "Poland", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Romania", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Food"},
-        {name: "Belarus", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Experience", tours: "Food"},
-        {name: "Greece", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Bulgaria", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Iceland", climate: "Cold", continent: "Eurasia", geo: "Nature", mood: "Experience", tours: "Museum"},
-        {name: "Hungary", climate: "Warm", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Food"},
-        {name: "Austria", climate: "Cold", continent: "Eurasia", geo: "City", mood: "Relax", tours: "Museum"}
-    );
-}
