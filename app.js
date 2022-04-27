7b138aeafeb6fb56a1a722dc691cc1597fe30f2a
Hursit Tarcan <75273616+HursitTarcan@users.noreply.github.com>
2022-04-27 15:36:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -6,8 +8,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Zeet node');
 });
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 
 server.listen(port, hostname, () => {
     console.log("Server running at http://0.0.0.0:3000/");
